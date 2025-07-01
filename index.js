094cb70f8ccbf27607ec66a2c2fbad995fa44672
Alex Ticona Mamani <alexticoma4@gmail.com>
2025-06-30 23:51:10 -0500

    cambio

--- index.js
+++ index.js
@@ -11,9 +11,215 @@ readdirSync("./routes").map((file) =>
 );
 
 app.get("/", (req, res) => {
-  res.send("Main page!");
-});
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8" />
+      <title>API</title>
+      <style>
+        body {
+         font-family: 'Segoe UI', sans-serif;
+          padding: 40px;
+          background-color: #f5f5f5;
+          color: #333;
+        }
+        h1 {
+          text-align: center;
+          color: #333;
+          margin-bottom: 40px;
+        }
+        .route-box {
+          background-color: white;
+          padding: 20px;
+          margin-bottom: 20px;
+          border-left: 5px solid #4caf50;
+          box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+          border-radius: 6px;
+        }
+        .title {
+           font-size: 20px;
+          font-weight: 600;
+          color: #222;
+          margin-bottom: 5px;
+        }
+        .method {
+          display: inline-block;
+          padding: 4px 10px;
+          font-size: 13px;
+          font-weight: bold;
+          color: white;
+          background-color: #4caf50;
+          border-radius: 4px;
+          margin-bottom: 10px;
+        }
+       .url a {
+         display: inline-block;
+          color: #007acc;
+          font-family: monospace;
+          font-size: 14px;
+          padding: 6px 10px;
+          background-color: #f8f8f8;
+        
+          border-radius: 6px;
+          text-decoration: none;
+          transition: all 0.2s ease-in-out;
+      }
 
+.url a:hover {
+  background-color: #e0f7ff;
+  border-color: #007acc;
+  color: #005a99;
+  box-shadow: 0 2px 6px rgba(0,0,0,0.1);
+}
+
+.controller{
+    color:rgb(231, 60, 60);
+}
+
+.hide{
+    display: none ;
+}
+      </style>
+    </head>
+    <body>
+      <h1> UTILITARIOS</h1>
+
+       <div class="route-box">
+        <div class="title">Obtener todas las categorías -> <span class="controller">getAllCategory</span></div>
+        <div class="method">GET</div>
+         <div class="url"><a href="/articles/getAllCategory"   target="_blank">/articles/getAllCategory</a></div>
+      </div>
+
+      <div class="route-box">
+        <div class="title">Obtener artículo por Slug {react,tailwind,angular} -> <span class="controller">getAllContentArticleByArticleSlug</span> </div>
+        <div class="method">GET</div>
+        <div class="url"><a href="/article/getAllContentArticleByArticleSlug/react"   target="_blank">/article/getAllContentArticleByArticleSlug/:slug</a></div>
+      </div>
+
+      <div class="route-box">
+        <div class="title">Obtener contenido de un articulo por slug {guia-completa-de-react-con-hooks} -> <span class="controller">getContentArticleByArticleSlug</span> </div>
+        <div class="method">GET</div>
+        <div class="url"><a href="/article/getContentArticleByArticleSlug/guia-completa-de-react-con-hooks"   target="_blank">/article/getContentArticleByArticleSlug/:slug</a></div>
+      </div>
+
+       <div class="route-box">
+        <div class="title">Obtener contenido de un articulo por slug {guia-completa-de-react-con-hooks} -> <span class="controller">getContentArticleByArticleSlug</span> </div>
+        <div class="method">GET</div>
+        <div class="url"><a href="/article/getAllContent/guia-completa-de-react-con-hooks"   target="_blank">/article/getAllContent/:slug</a></div>
+      </div>
+      
+      <div class="route-box">
+        <div class="title">Obtener todos los contenidos globales paginados con filtros -><span class="controller">getAllContent</span></div>
+        <div class="method">GET</div>
+        <div class="url"><a href="/article/getAllContent?page=1&limit=10" target="_blank">/article/getAllContent?page=1&limit=10 </a></div>
+      </div>
+
+      <!-- Filtrar por tag: react -->
+<div class="route-box">
+  <div class="title">Filtrar por tag (react)</div>
+  <div class="method">GET</div>
+  <div class="url">
+    <a href="/article/getAllContent?tag=react" target="_blank">
+      /article/getAllContent?tag=react
+    </a>
+  </div>
+</div>
+
+<!-- Filtrar por categoría (slug): angular -->
+<div class="route-box">
+  <div class="title">Filtrar por categoría (angular)</div>
+  <div class="method">GET</div>
+  <div class="url">
+    <a href="/article/getAllContent?slug=angular" target="_blank">
+      /article/getAllContent?slug=angular
+    </a>
+  </div>
+</div>
+
+<!-- Búsqueda por palabra clave: "hooks" -->
+<div class="route-box">
+  <div class="title">Buscar por palabra clave ("hooks")</div>
+  <div class="method">GET</div>
+  <div class="url">
+    <a href="/article/getAllContent?search=hooks" target="_blank">
+      /article/getAllContent?search=hooks
+    </a>
+  </div>
+</div>
+
+<!-- Ordenar por fecha ascendente -->
+<div class="route-box">
+  <div class="title">Ordenar por fecha ascendente</div>
+  <div class="method">GET</div>
+  <div class="url">
+    <a href="/article/getAllContent?sort=asc" target="_blank">
+      /article/getAllContent?sort=asc
+    </a>
+  </div>
+</div>
+
+<!-- Combinación de filtros: categoría=react + búsqueda="estado" + paginación -->
+<div class="route-box">
+  <div class="title">Combinación: react + estado + paginación</div>
+  <div class="method">GET</div>
+  <div class="url">
+    <a href="/article/getAllContent?slug=react&search=estado&page=1&limit=5" target="_blank">
+      /article/getAllContent?slug=react&search=estado&page=1&limit=10
+    </a>
+  </div>
+</div>
+
+
+
+
+
+    </body>
+    </html>
+  `);
+});
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+{
+  /* 
+
+
+  
+      <div class="route-box">
+        <div class="title">Obtener contenido de un articulo por paginado (slug)</div>
+        <div class="method">GET</div>
+        <div class="url"><a href="/article/content/paginate/react"   target="_blank">/article/content/paginate/:slug </a></div>
+      </div>
+
+      <div class="route-box">
+        <div class="title">Obtener contenido de un articulo por paginado (slug)</div>
+        <div class="method">GET</div>
+        <div class="url"><a href="/article/content/react/guia-completa-de-react-con-hooks"   target="_blank">/article/content/paginate/:slug </a></div>
+      </div>
+      
+
+
+ Obtener todos los artículos
+  <div class="route-box hide">
+<div class="title">Obtener todos los artículos sin paginar</div>
+<div class="method">GET</div>
+ <div class="url"><a href="/articles"  target="_blank">/articles</a></div>
+</div>
+ Obtener artículo por ID
+<div class="route-box hide">
+<div class="title">Obtener artículo por ID</div>
+<div class="method">GET</div>
+<div class="url"><a href="/article/1"   target="_blank">/article/:id</a></div>
+</div> 
+Obtener artículos por categoría (slug)
+
+      <div class="route-box">
+        <div class="title">Obtener todos los artículos por categoría (slug)</div>
+        <div class="method">GET</div>
+        <div class="url"><a href="/articles/category/react"   target="_blank">/articles/category/:category</a></div>
+      </div>
+
+*/
+}
--- routes/article.js
+++ routes/article.js
@@ -3,18 +3,17 @@ const router = express.Router();
 
 let article = [
   {
-    id: "1",
+    id: 1,
     title: "react",
     type: "frontend",
     slug: "react",
-    icon: "https://example.com/icons/react.png",
     contenido: [
       {
-        id: "1",
+        id: 1,
         titulo: "Guía completa de React con Hooks",
         descripcion:
           "Introducción a React con ejemplos prácticos usando Hooks.",
-        uri: "/guia-completa-de-react-con-hooks",
+        slug: "/guia-completa-de-react-con-hooks",
         contenido: "Aquí va el contenido extenso o resumido de la guía...",
         tags: ["react", "hooks", "javascript"],
         fecha: "2023-10-01 12:00:00",
@@ -30,11 +29,11 @@ let article = [
         ]
       },
       {
-        id: "2",
+        id: 2,
         titulo: "Estado global con Context API",
         descripcion:
           "Uso de React Context para manejar el estado global de la aplicación.",
-        uri: "/uso-de-context-api-en-react",
+        slug: "/uso-de-context-api-en-react",
         contenido: "Explicación de cómo funciona Context...",
         tags: ["react", "estado", "context"],
         fecha: "2023-11-10 08:45:00",
@@ -48,17 +47,16 @@ let article = [
     ]
   },
   {
-    id: "2",
+    id: 2,
     title: "angular",
     type: "frontend",
     slug: "angular",
-    icon: "https://example.com/icons/react.png",
     contenido: [
       {
-        id: "1",
+        id: 1,
         titulo: "Introducción a Angular y componentes",
         descripcion: "Crea componentes reutilizables en Angular paso a paso.",
-        uri: "/guias/angular/componentes-basicos",
+        slug: "/guias/angular/componentes-basicos",
         contenido:
           "Angular es un framework para crear SPA utilizando TypeScript...",
         tags: ["angular", "componentes", "typescript"],
@@ -73,17 +71,16 @@ let article = [
     ]
   },
   {
-    id: "3",
+    id: 3,
     title: "vue",
     type: "frontend",
     slug: "vue",
-    icon: "https://example.com/icons/react.png",
     contenido: [
       {
         id: "1",
         titulo: "Primeros pasos con Vue 3",
         descripcion: "Cómo empezar a usar Vue.js 3 con Composition API.",
-        uri: "/guias/vue/vue3-intro",
+        slug: "/guias/vue/vue3-intro",
         contenido:
           "Vue 3 utiliza una nueva Composition API que mejora la organización del código...",
         tags: ["vue", "javascript", "composition-api"],
@@ -98,17 +95,16 @@ let article = [
     ]
   },
   {
-    id: "4",
+    id: 4,
     title: "prisma",
     type: "orm",
     slug: "prisma",
-    icon: "https://example.com/icons/react.png",
     contenido: [
       {
-        id: "1",
+        id: 1,
         titulo: "Primeros pasos con Prisma ORM",
         descripcion: "Cómo configurar Prisma en un proyecto Node.js",
-        uri: "/guias/prisma/primeros-pasos",
+        slug: "/guias/prisma/primeros-pasos",
         contenido: "Comienza inicializando el esquema con `npx prisma init`...",
         tags: ["prisma", "orm", "node"],
         fecha: "2024-02-15 16:00:00",
@@ -122,17 +118,16 @@ let article = [
     ]
   },
   {
-    id: "5",
+    id: 5,
     title: "node",
     type: "frontend",
     slug: "node",
-    icon: "https://example.com/icons/react.png",
     contenido: [
       {
-        id: "1",
+        id: 1,
         titulo: "Servidor básico con Express.js",
         descripcion: "Aprende a levantar un servidor con Node y Express.",
-        uri: "/guias/node/express-basico",
+        slug: "/guias/node/express-basico",
         contenido:
           "Express es un framework minimalista para Node.js que facilita la creación de APIs...",
         tags: ["node", "express", "api"],
@@ -147,17 +142,16 @@ let article = [
     ]
   },
   {
-    id: "6",
+    id: 6,
     title: "typescript",
     type: "frontend",
     slug: "typescript",
-    icon: "https://example.com/icons/react.png",
     contenido: [
       {
         id: "1",
         titulo: "Fundamentos de TypeScript",
         descripcion: "Tipos, interfaces y clases en TypeScript.",
-        uri: "/guias/typescript/fundamentos",
+        slug: "/guias/typescript/fundamentos",
         contenido:
           "TypeScript extiende JavaScript con tipado estático opcional...",
         tags: ["typescript", "tipado", "clases", "interfaces"],
@@ -171,165 +165,328 @@ let article = [
       }
     ]
   },
-
   {
-    id: "7",
+    id: 7,
     title: "csharp",
     type: "backend",
     slug: "csharp",
-    icon: "https://example.com/icons/csharp.png",
     contenido: []
   },
   {
-    id: "8",
+    id: 8,
     title: "go",
     type: "backend",
     slug: "go",
-    icon: "https://example.com/icons/go.png",
     contenido: []
   },
 
   {
-    id: "9",
+    id: 9,
     title: "sql",
     type: "base de datos",
     slug: "sql",
-    icon: "https://example.com/icons/sql.png",
     contenido: []
   },
   {
-    id: "10",
+    id: 10,
     title: "ui/ux",
     type: "diseño",
     slug: "uiux",
-    icon: "https://example.com/icons/uiux.png",
     contenido: []
   },
   {
-    id: "11",
+    id: 11,
     title: "tailwind",
     type: "frontend",
     slug: "tailwind",
-    icon: "https://example.com/icons/tailwind.png",
     contenido: []
   },
   {
-    id: "12",
+    id: 12,
     title: "herramientas",
     type: "utilidades",
     slug: "herramientas",
-    icon: "https://example.com/icons/sql.png",
     contenido: []
   },
   {
-    id: "13",
+    id: 13,
     title: "nextjs",
     type: "fullstack",
     slug: "nextjs",
-    icon: "https://example.com/icons/nextjs.png",
     contenido: []
   },
   {
-    id: "14",
+    id: 14,
     title: "nestjs",
     type: "backend",
     slug: "nestjs",
-    icon: "https://example.com/icons/nestjs.png",
     contenido: []
   },
   {
-    id: "15",
+    id: 15,
     title: "flutter",
     type: "mobile",
     slug: "flutter",
-    icon: "https://example.com/icons/flutter.png",
     contenido: []
   },
   {
-    id: "16",
+    id: 16,
     title: "react native",
     type: "mobile",
     slug: "react-native",
-    icon: "https://example.com/icons/react-native.png",
     contenido: []
   },
   {
-    id: "17",
+    id: 17,
     title: "otros",
     type: "utilidades",
     slug: "otros",
-    icon: "https://example.com/icons/sql.png",
     contenido: []
   }
 ];
 
-// const getArticle = (req, res) => {
-//   res.json(article);
-// };
+// Obtener todas las categorías
+router.get("/articles/getAllCategory", (req, res) => {
+  const categories = article.map(
+    ({ id, title, type, icon, slug, contenido }) => ({
+      id,
+      title,
+      type,
+      slug,
+      total: contenido.length,
+      icon
+    })
+  );
 
-// const getArticleById = (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "Lista de categorías",
+    total: categories.length,
+    data: categories
+  });
+});
+
+//  Obtener artículo por Slug (getAllContentArticleByArticleSlug())
+router.get("/article/getAllContentArticleByArticleSlug/:slug", (req, res) => {
+  const result = article.find((b) => b.slug === req.params.slug);
+  if (!result) {
+    return res.status(404).json({
+      status: 404,
+      message: "Artículo por slug no encontrado",
+      data: null
+    });
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Contenido del artículo ${req.params.slug}`,
+    total: result.contenido.length,
+    data: result.contenido
+  });
+});
+
+// Buscar contenido por slug de contenido interno
+router.get("/article/getContentArticleByArticleSlug/:slug", (req, res) => {
+  const slugParam = "/" + req.params.slug;
+
+  for (const art of article) {
+    const contenido = art.contenido.find((c) => c.slug === slugParam);
+    if (contenido) {
+      return res.status(200).json({
+        status: 200,
+        message: "Contenido encontrado",
+        data: contenido
+      });
+    }
+  }
+
+  return res.status(404).json({
+    status: 404,
+    message: "Contenido no encontrado para el slug proporcionado",
+    data: null
+  });
+});
+
+router.get("/article/getAllContent", (req, res) => {
+  const { page = 1, limit = 10, tag, slug, search, sort = "desc" } = req.query;
+
+  // 1. Juntar todos los contenidos con metadatos de su categoría
+  let allContent = article.flatMap((a) =>
+    a.contenido.map((c) => ({
+      ...c,
+      categoria: a.title,
+      categoriaSlug: a.slug
+    }))
+  );
+
+  // 2. Filtrar por tag
+  if (tag) {
+    allContent = allContent.filter((c) => c.tags.includes(tag));
+  }
+
+  // 3. Filtrar por categoría (slug del artículo)
+  if (slug) {
+    allContent = allContent.filter((c) => c.categoriaSlug === slug);
+  }
+
+  // 4. Buscar por texto (en título, descripción o slug del contenido)
+  if (search) {
+    const q = search.toLowerCase();
+    allContent = allContent.filter(
+      (c) =>
+        c.titulo.toLowerCase().includes(q) ||
+        c.descripcion.toLowerCase().includes(q) ||
+        c.slug.toLowerCase().includes(q)
+    );
+  }
+
+  // 5. Ordenar por fecha
+  allContent.sort((a, b) => {
+    const fechaA = new Date(a.fecha).getTime();
+    const fechaB = new Date(b.fecha).getTime();
+    return sort === "asc" ? fechaA - fechaB : fechaB - fechaA;
+  });
+
+  // 6. Paginación
+  const total = allContent.length;
+  const totalPages = Math.ceil(total / limit);
+  const startIndex = (page - 1) * limit;
+  const paginated = allContent.slice(startIndex, startIndex + Number(limit));
+
+  // 7. Respuesta
+  res.status(200).json({
+    status: 200,
+    message: "Contenido global paginado",
+    total,
+    currentPage: Number(page),
+    totalPages,
+    data: paginated
+  });
+});
+
+// router.get("/article/content/paginate/:slug", (req, res) => {
+//   const { slug } = req.params;
+//   const page = parseInt(req.query.page) || 1;
+//   const limit = 1; // mostramos de a 1 contenido por página
+
+//   const articleFound = article.find((b) => b.slug === slug);
+//   if (!articleFound || articleFound.contenido.length === 0) {
+//     return res.status(404).json({
+//       status: 404,
+//       message: "Artículo no encontrado o sin contenido",
+//       data: null
+//     });
+//   }
+
+//   const total = articleFound.contenido.length;
+//   const totalPages = Math.ceil(total / limit);
+
+//   if (page < 1 || page > totalPages) {
+//     return res.status(400).json({
+//       status: 400,
+//       message: "Página fuera de rango",
+//       data: null
+//     });
+//   }
+
+//   const startIndex = (page - 1) * limit;
+//   const contenidoPage = articleFound.contenido.slice(
+//     startIndex,
+//     startIndex + limit
+//   );
+
+//   res.status(200).json({
+//     status: 200,
+//     message: "Contenido paginado del artículo",
+//     currentPage: page,
+//     totalPages,
+//     totalItems: total,
+//     data: contenidoPage
+//   });
+// });
+
+// router.get("/article/content/:categoriaSlug/:contenidoSlug", (req, res) => {
+//   const { categoriaSlug, contenidoSlug } = req.params;
+
+//   const categoria = article.find((a) => a.slug === categoriaSlug);
+//   if (!categoria) {
+//     return res.status(404).json({
+//       status: 404,
+//       message: "Categoría no encontrada",
+//       data: null
+//     });
+//   }
+
+//   const contenido = categoria.contenido.find(
+//     (c) => c.slug.replace("/", "") === contenidoSlug
+//   );
+
+//   if (!contenido) {
+//     return res.status(404).json({
+//       status: 404,
+//       message: "Contenido no encontrado",
+//       data: null
+//     });
+//   }
+
+//   res.status(200).json({
+//     status: 200,
+//     message: "Contenido encontrado",
+//     data: {
+//       categoria: categoria.title,
+//       contenido
+//     }
+//   });
+// });
+
+module.exports = router;
+
+// Obtener todos los artículos
+// router.get("/articles", (req, res) => {
+//   res.status(200).json({
+//     status: 200,
+//     message: "Lista de artículos",
+//     total: article.length,
+//     data: article
+//   });
+// });
+
+// // Obtener artículo por ID
+// router.get("/article/:id", (req, res) => {
 //   const result = article.find((b) => b.id === req.params.id);
-//   if (!result) return res.status(404).json({ error: "articulo no encontrada" });
-//   res.json(result.contenido);
-// };
+//   if (!result) {
+//     return res.status(404).json({
+//       status: 404,
+//       message: "Artículo no encontrado",
+//       data: null
+//     });
+//   }
 
-// const getArticleBySlug = (req, res) => {
-//   const result = article.find((b) => b.slug === req.params.slug);
-//   if (!result)
-//     return res.status(404).json({ error: "articulo por slug no encontrada" });
-//   res.json(result.contenido);
-// };
+//   res.status(200).json({
+//     status: 200,
+//     message: "Contenido del artículo encontrado",
+//     total: result.contenido.length,
+//     data: result.contenido
+//   });
+// });
 
-// const getArticlesByCategory = (req, res) => {
-//   const categories = article.map(({ id, title, type, icon }) => ({
-//     id,
-//     title,
-//     type,
-//     icon
-//   }));
-//   res.json(categories);
-// };
-
-// const getarticleByCategorySlug = (req, res) => {
+// // Obtener artículos por categoría (slug)
+// router.get("/articles/category/:category", (req, res) => {
 //   const slug = req.params.category;
 //   const result = article.filter((b) => b.slug === slug);
-//   if (result.length === 0)
-//     return res
-//       .status(404)
-//       .json({ error: "No se encontraron artículos en esta categoría" });
-//   res.json(result);
-// };
 
-router.get("/articles", (req, res) => {
-  res.send(article);
-});
-router.get("/article/:id", (req, res) => {
-  const result = article.find((b) => b.id === req.params.id);
-  if (!result) return res.status(404).json({ error: "articulo no encontrada" });
-  res.json(result.contenido);
-});
-router.get("/article/slug/:slug", (req, res) => {
-  const result = article.find((b) => b.slug === req.params.slug);
-  if (!result)
-    return res.status(404).json({ error: "articulo por slug no encontrada" });
-  res.json(result.contenido);
-});
-router.get("/articles/category", (req, res) => {
-  const categories = article.map(({ id, title, type, icon }) => ({
-    id,
-    title,
-    type,
-    icon
-  }));
-  res.json(categories);
-});
-router.get("/articles/category/:category", (req, res) => {
-  const slug = req.params.category;
-  const result = article.filter((b) => b.slug === slug);
-  if (result.length === 0)
-    return res
-      .status(404)
-      .json({ error: "No se encontraron artículos en esta categoría" });
-  res.json(result);
-});
-module.exports = router;
+//   if (result.length === 0) {
+//     return res.status(404).json({
+//       status: 404,
+//       message: "No se encontraron artículos en esta categoría",
+//       data: null
+//     });
+//   }
+
+//   res.status(200).json({
+//     status: 200,
+//     message: "Artículos por categoría",
+//     total: result.length,
+//     data: result
+//   });
+// });
